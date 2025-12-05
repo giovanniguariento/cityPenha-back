b130782b038aee6e7c4d171d39f82943eb706758
Kleisson <kleissonsm.dev@gmail.com>
2025-12-05 15:44:08 -0300

    feat: consertado ip do server

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import app from './app';
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(3000, "0.0.0.0", () => {
+app.listen(3000, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
